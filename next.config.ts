2729b37df3dbbd2007ac86cb43ea792bf00968e7
Walid Bezoui <bezouiw@gmail.com>
2025-05-09 16:30:02 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Invalid src prop (https://images.openfoodfacts.org/images/products/611/103/500/0430/front_en.104.200.jpg) on `next/image`, hostname "images.openfoodfacts.org" is not configured under images in your `next.config.js`
    > See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host. Error source: src/components/product/ProductCard.tsx (18:13) @ ProductCard
    >
    >   16 |         <div className="aspect-[4/3] relative w-full bg-muted">
    >   17 |           {product.imageUrl ? (
    > > 18 |             <Image
    >      |             ^
    >   19 |               src={product.imageUrl}
    >   20 |               alt={product.name}
    >   21 |               fill
    >
    > Call Stack
    > 10
    >
    > Show 5 ignore-listed frame(s)
    > Array.map
    > <anonymous> (0:0)
    > ProductCard
    > src/components/product/ProductCard.tsx (18:13)
    > <anonymous>
    > src/app/search/page.tsx (33:9)
    > SearchResults
    > src/app/search/page.tsx (32:17)
    > SearchPage
    > src/app/search/page.tsx (59:18)

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -16,6 +17,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.openfoodfacts.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.openfoodfacts.org',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
